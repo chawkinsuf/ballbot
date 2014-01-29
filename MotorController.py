84209272aa848ccff275b4ea8cb8be5264b28b79
chawkinsuf <chawkinsuf@gmail.com>
2014-01-29 14:21:15 -0500

    Working on the motor controller

--- MotorController.py
+++ MotorController.py
@@ -1,4 +1,5 @@
 import time, atexit
+from flufl.enum import Enum
 import RPi.GPIO as GPIO
 from Adafruit_PWM_Servo_Driver import PWM
 from devtools import rangetest
@@ -14,7 +15,7 @@ R_GPIO = 25
 L_PWM = 14
 R_PWM = 15
 
-SPEED_MAX = 0
+SPEED_MAX = 1
 SPEED_MIN = 950
 
 
@@ -35,59 +36,58 @@ class MotorController:
 		self.pwm = PWM( PWM_ADDRESS )
 		self.pwm.setPWMFreq( PWM_FREQUENCY )
 
+		# Initialize variables
+		self.speed = 0
+
 	def _exit_handler( self ):
 		self.stop()
 		GPIO.cleanup()
 
 	def _convert_speed( self, speed ):
-		return ( SPEED_MAX - SPEED_MIN ) * speed + SPEED_MIN
+		return int( ( SPEED_MAX - SPEED_MIN ) * speed + SPEED_MIN )
+
+	@rangetest( speed = (-1.0, 1.0) )
+	def run( self, speed ):
+
+		# Store the current speed
+		self.speed = speed
+
+		# See if we need to stop
+		if ( speed == 0 ):
+			self.stop()
+			return
+
+		# Get the direction
+		forward = ( speed > 0 )
+
+		# Get the pulse length from our speed
+		speed = abs( speed )
+		pulse = self._convert_speed( speed )
+
+		# Adjust the pulse for direction
+		if ( not forward ):
+			pulse = PWM_RANGE - pulse
+
+		# Set the speed and direction
+		self.pwm.setPWM( L_PWM, 0, pulse )
+		self.pwm.setPWM( R_PWM, 0, pulse )
+
+		# Turn on the motors
+		self.start()
+
+	def turn( self ):
+
+		# Set the speed and direction
+		self.pwm.setPWM( L_PWM, 0, 1 )
+		self.pwm.setPWM( R_PWM, 0, PWM_RANGE - 1 )
+
+		# Turn on the motors
+		self.start()
+
+	def start( self ):
+		GPIO.output( L_GPIO, GPIO.HIGH )
+		GPIO.output( R_GPIO, GPIO.HIGH )
 
 	def stop( self ):
 		GPIO.output( L_GPIO, GPIO.LOW )
 		GPIO.output( R_GPIO, GPIO.LOW )
-
-	@rangetest( speed = (0.0, 1.0) )
-	def forward( self, speed, stop = None ):
-
-		# Get the pulse length from our speed
-		pulse = self._convert_speed( speed )
-
-		# Set the speed and direction
-		pwm.setPWM( L_PWM, 0, pulse )
-		pwm.setPWM( R_PWM, 0, pulse )
-
-		# Turn the motors on
-		GPIO.output( L_GPIO, GPIO.HIGH )
-		GPIO.output( R_GPIO, GPIO.HIGH )
-
-		if ( not stop is None):
-
-			# Sleep the specified time
-			time.sleep( stop )
-
-			# Turn off the motors
-			GPIO.output( L_GPIO, GPIO.LOW )
-			GPIO.output( R_GPIO, GPIO.LOW )
-
-	@rangetest( speed = (0.0, 1.0) )
-	def reverse( self, speed, stop = None ):
-
-		# Get the pulse length from our speed
-		pulse = self._convert_speed( speed )
-
-		# Set the speed and direction
-		pwm.setPWM( L_PWM, 0, PWM_RANGE - pulse )
-		pwm.setPWM( R_PWM, 0, PWM_RANGE - pulse )
-
-		# Turn the motors on
-		GPIO.output( L_GPIO, GPIO.HIGH )
-		GPIO.output( R_GPIO, GPIO.HIGH )
-
-		if ( not stop is None):
-
-			# Sleep the specified time
-			time.sleep( stop )
-
-			# Turn off the motors
-			GPIO.output( L_GPIO, GPIO.LOW )
-			GPIO.output( R_GPIO, GPIO.LOW )
--- main.py
+++ main.py
@@ -1,90 +1,20 @@
 #!/usr/bin/python
 
-from Adafruit_PWM_Servo_Driver import PWM
-import RPi.GPIO as GPIO
+from MotorController import MotorController
 import time, signal, sys
-
-L_GPIO = 24
-R_GPIO = 25
-
-L_PWM = 14
-R_PWM = 15
 
 
 def signal_handler( signal, frame ):
-
-	# Stop the motors
-	GPIO.output( L_GPIO, GPIO.LOW )
-	GPIO.output( R_GPIO, GPIO.LOW )
-
-	# Exit
-	GPIO.cleanup()
 	sys.exit(0)
 
 
 # Capture control-c presses
 signal.signal( signal.SIGINT, signal_handler )
 
-# Initialize the gpio pins
-GPIO.setmode( GPIO.BCM )
-GPIO.setup( L_GPIO, GPIO.OUT )
-GPIO.setup( R_GPIO, GPIO.OUT )
+motors = MotorController()
 
-# Initialize the pwn board
-pwm = PWM( 0x40 )
-pwm.setPWMFreq( 1000 )
-
-# Enable both motors
-GPIO.output( L_GPIO, GPIO.HIGH )
-GPIO.output( R_GPIO, GPIO.HIGH )
-
-# Test forward speeds
-print('Forward')
-for i in range(0,1000,10):
-	pwm.setPWM( L_PWM, 0, i )
-	pwm.setPWM( R_PWM, 0, i )
-	time.sleep( 0.05 )
-	if i % 50 == 0:
-		print( i )
-
-# Test reverse speeds
-print('Reverse')
-for i in range(0,1000,10):
-	pwm.setPWM( L_PWM, 0, 4096-i )
-	pwm.setPWM( R_PWM, 0, 4096-i )
-	time.sleep( 0.05 )
-	if i % 50 == 0:
-		print( i )
-
-time.sleep(2)
-
-# Forward full
-pwm.setPWM( L_PWM, 0, 4096 )
-pwm.setPWM( R_PWM, 0, 4096 )
-
-time.sleep(2)
-
-# Forward backward
-pwm.setPWM( L_PWM, 4096, 0 )
-pwm.setPWM( R_PWM, 4096, 0 )
-
-time.sleep(2)
-
-# Turn left
-pwm.setPWM( L_PWM, 0, 4096 )
-pwm.setPWM( R_PWM, 4096, 0 )
-
-time.sleep(2)
-
-# Turn right
-pwm.setPWM( L_PWM, 4096, 0 )
-pwm.setPWM( R_PWM, 0, 4096 )
-
-time.sleep(2)
-
-# Stop the motors
-GPIO.output( L_GPIO, GPIO.LOW )
-GPIO.output( R_GPIO, GPIO.LOW )
-
-# Exit
-GPIO.cleanup()
+motors.run( 1 )
+time.sleep( 2 )
+motors.run( -1 )
+time.sleep( 2 )
+motors.run( 0 )
