fb55e97d6d32e267a0eb504ea13dcb063817d44b
chawkinsuf <chawkinsuf@gmail.com>
2014-02-04 02:05:21 -0500

    Add tests for the compass module

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
-from MotorController import MotorController
+# from MotorController import MotorController
+from Compass import Compass
+from pprint import pprint
 import time, signal, sys
 
 
@@ -11,10 +13,38 @@ def signal_handler( signal, frame ):
 # Capture control-c presses
 signal.signal( signal.SIGINT, signal_handler )
 
-motors = MotorController()
 
-motors.run( 1 )
-time.sleep( 2 )
-motors.run( -1 )
-time.sleep( 2 )
-motors.run( 0 )
+
+
+compass = Compass( interrupt = 25 )
+compass.setConfig( rate = Compass.Config.Rate.Hz_03_00, samples = Compass.Config.Samples.Two, gain = Compass.Config.Gain.Ga_0_9 )
+
+# compass.singleMeasurement()
+# compass.singleMeasurement()
+# compass.singleMeasurement()
+# time.sleep(2)
+
+compass.run()
+time.sleep(4)
+print(compass.interrupt_count)
+compass.stop()
+time.sleep(2)
+compass.run()
+time.sleep(4)
+print(compass.interrupt_count)
+
+
+
+# motors = MotorController()
+
+# time.sleep(10)
+# print( motors.count )
+# sys.exit(0)
+
+# motors.run( 1 )
+# time.sleep( 2 )
+# # motors.run( -1 )
+# time.sleep( 2 )
+# motors.run( 0 )
+
+# print( motors.count )
